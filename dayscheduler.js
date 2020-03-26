ce156ef3037971e6534122b86f36c70864f3372b
Janice Leung <janice.leung2008@gmail.com>
2020-03-26 22:44:15 +1100

    highlight time function

--- dayscheduler.js
+++ dayscheduler.js
@@ -1,6 +1,6 @@
 
-
 $(document).ready(function() {
+
 //Show date on HTML
 var now = moment().format('MMMM Do YYYY');
 
@@ -85,7 +85,6 @@ $("#save-one").on("click", function () {
 
 $("#text-one").val(localStorage.getItem("storedText-one"));
 $("#save-one").on("click", function () {
- 
     var textEl = $("#text-one").val();
     //store input
     localStorage.setItem("storedText-one", textEl);
@@ -139,6 +138,100 @@ $("#save-five").on("click", function () {
     var storedEl = $("#text-five").innerHTML = localStorage.getItem("storedText-five");    
     console.log(storedEl + " works")
 });
+
+// colours for time 
+var currentTime = moment().format('h a')
+var time = [
+  {
+    h : 9,
+    a : "am"
+  },
+  {
+    h : 10,
+    a : "am"
+  },
+  {
+    h : 11,
+    a : "am"
+  },
+  {
+    h : 12,
+    a : "pm"
+  },
+  {
+    h : 1,
+    a : "pm"
+  },
+  {
+    h : 2,
+    a : "pm"
+  },
+  {
+    h : 3,
+    a : "pm"
+  },
+  {
+    h : 4,
+    a : "pm"
+  },
+  {
+    h : 5,
+    a : "pm"
+  },
+  
+]
+
+$("#nine").text(time[0].h +" "+ time[0].a);
+$("#ten").text(time[1].h +" "+ time[1].a);
+$("#eleven").text(time[2].h +" "+ time[2].a);
+$("#twelve").text(time[3].h +" "+ time[3].a)
+$("#one").text(time[4].h +" "+ time[4].a)
+$("#two").text(time[5].h +" "+ time[5].a)
+$("#three").text(time[6].h +" "+ time[6].a)
+$("#four").text(time[7].h +" "+ time[7].a)
+$("#five").text(time[8].h +" "+ time[8].a)
+
+console.log(time[0].h +" "+ time[0].a)
+console.log(time[1].h +" "+ time[1].a)
+console.log(time[8].h +" "+ time[8].a)
+console.log(currentTime);
+
+
+if (currentTime === time[0].h +" "+ time[0].a) {
+  $(".attribute-nine").attr("class", "col-md-8 border bg-success text-white");
+} 
+
+if (currentTime === time[1].h +" "+ time[1].a) {
+  $(".attribute-ten").attr("class", "col-md-8 border bg-success text-white");
+} 
+
+if (currentTime === time[2].h +" "+ time[2].a) {
+  $(".attribute-eleven").attr("class", "col-md-8 border bg-success text-white");
+} 
+
+if (currentTime === time[3].h +" "+ time[3].a) {
+  $(".attribute-twelve").attr("class", "col-md-8 border bg-success text-white");
+} 
+
+if (currentTime === time[4].h +" "+ time[4].a) {
+  $(".attribute-one").attr("class", "col-md-8 border bg-success text-white");
+} 
+
+if (currentTime === time[5].h +" "+ time[5].a) {
+  $(".attribute-two").attr("class", "col-md-8 border bg-success text-white");
+} 
+
+if (currentTime === time[6].h +" "+ time[6].a) {
+  $(".attribute-three").attr("class", "col-md-8 border bg-success text-white");
+} 
+
+if (currentTime === time[7].h +" "+ time[7].a) {
+  $(".attribute-four").attr("class", "col-md-8 border bg-success text-white");
+} 
+
+if (currentTime === time[8].h +" "+ time[8].a) {
+  $(".attribute-five").attr("class", "col-md-8 border bg-success text-white");
+} 
 
 
 
